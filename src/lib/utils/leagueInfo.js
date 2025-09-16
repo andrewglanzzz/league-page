0ca0bebe9c4707525e264a309eee332349c8d878
Andrew Glanz <andrewvita0@gmail.com>
2025-09-16 00:41:42 -0400

    case sensitivity

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -92,7 +92,7 @@ export const managers = [
       "tookOver": 2024, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Ohio", // (optional)
       "bio": "pending",
-      "photo": "/managers/Steven.png", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/steven.png", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "cin", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
@@ -180,7 +180,7 @@ export const managers = [
       "tookOver": 2024, // (optional) used if a manager took over a team, delete this line or change to null otherwise
       "location": "Ohio", // (optional)
       "bio": "pending",
-      "photo": "/managers/Joel.jpg", // square ratio recommended (no larger than 500x500)
+      "photo": "/managers/joel.jpg", // square ratio recommended (no larger than 500x500)
       "fantasyStart": 2024, // (optional) when did the manager start playing fantasy football
       "favoriteTeam": "cin", // (optional) favorite NFL team, (follows convention: nyj, sea, mia, etc.) MUST BE LOWERCASE
       "mode": "Win Now", // (optional) 'Win Now', 'Dynasty', or 'Rebuild' (anything else and you will need to add a new png to /static/ similar to the 'Rebuild.png' and 'Win Now.png' currently in there)
